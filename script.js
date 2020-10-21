e05107f8a1fe6e9f505b81269a76800c61e7dab3
BorislavShutov <oryouormehoney@gmail.com>
2020-10-21 14:08:13 +0300

    addEllements

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ alert('Ворон и Ворона - это разные виды птиц!)');
 
 const hello = document.querySelector('.hello-item'),
 	  myText = document.querySelector('header'),
-	  addElement = document.querySelector('body');
+	  addElement = document.querySelector('body'),
+	  newBlock = document.querySelector('.new-block'),
+	  footerBlock = document.querySelector('footer');
 	  
 	  
 	   
@@ -16,10 +18,10 @@ myText.style.cssText = 'background-color : #fcb;';
 
 
 myText.insertAdjacentHTML('afterbegin', '<h1><strong>JS Bro</strong></h1>');
-addElement.insertAdjacentHTML('beforeend', '<h3><strong>New Block</strong></h3>');
-addElement.insertAdjacentHTML('beforeend', '<img src="img/1.jpg" alt="foto"></im>');
-addElement.style.cssText = 'background-color: #fff; text-align: center; color: #000;';
+newBlock.insertAdjacentHTML('afterbegin', '<img src="img/1.jpg" alt="name">');
+newBlock.style.cssText = 'background-color : #0008; color : #fff; display : flex; justify-content : center; flex-flow : column; padding : 2em;';
+footerBlock.insertAdjacentHTML('afterbegin', '<p>Contacts: +343 23 345 65 44</p>');
 
-
+footerBlock.style.cssText = 'font-size: 22px; text-align: center; padding: 2em;background-color: #fcbe05;';
 
 
